4bb3b10bf75a5af2b841d47323d82ca35ae53f56
Paul <koncewiczpaul@gmail.com>
2024-04-24 22:36:22 +0200

    Edit styles

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -15,22 +15,34 @@
     <body>
         <div class="content">
             <div class="header">
-                <div class="header-elements">
-                    <p>Sklep</p>
-                    <button class="log-in">
-                        <svg xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" version="1.1" width="256" height="256" viewBox="0 0 256 256" xml:space="preserve">
-                            <g style="stroke: none; stroke-width: 0; stroke-dasharray: none; stroke-linecap: butt; stroke-linejoin: miter; stroke-miterlimit: 10; fill: white; fill-rule: nonzero; opacity: 1;" transform="translate(1.4065934065934016 1.4065934065934016) scale(2.81 2.81)" >
+                <div class="row">
+                    <div class="col-lg-6">
+                        <p>Sklep</p>
+                    </div>
+                    <div class="col-lg-6">
+                        <button class="log-in">
+                            <svg xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" version="1.1" width="256" height="256" viewBox="0 0 256 256" xml:space="preserve" style="width: 40%; height: 46px;">
+                                <g style="stroke: none; stroke-width: 0; stroke-dasharray: none; stroke-linecap: butt; stroke-linejoin: miter; stroke-miterlimit: 10; fill: white; fill-rule: nonzero; opacity: 1;" transform="translate(1.4065934065934016 1.4065934065934016) scale(2.81 2.81)" >
                                 <path d="M 85.347 90 c 0 -22.283 -18.064 -40.347 -40.347 -40.347 S 4.653 67.717 4.653 90 H 85.347 z" style="stroke: none; stroke-width: 1; stroke-dasharray: none; stroke-linecap: butt; stroke-linejoin: miter; stroke-miterlimit: 10; fill: rgb(0,0,0); fill-rule: nonzero; opacity: 1;" transform=" matrix(1 0 0 1 0 0) " stroke-linecap="round" />
                                 <circle cx="45.003" cy="21.413" r="21.413" style="stroke: none; stroke-width: 1; stroke-dasharray: none; stroke-linecap: butt; stroke-linejoin: miter; stroke-miterlimit: 10; fill: rgb(0,0,0); fill-rule: nonzero; opacity: 1;" transform="  matrix(1 0 0 1 0 0) "/>
                             </g>
-</svg>
-                    </button>
-
+                            </svg>
+                        </button>
+                    </div>
                 </div>
             </div>
 
             <div class="main-content">
-                <div class="aside"></div>
+                <div class="aside">
+                    <h5>Kategorie</h5>
+                    <ul>
+                        <li><a  href="#" class="nav-link active" aria-current="page">Buty</a></li>
+                        <li><a  href="#" class="nav-link active" aria-current="page">Piłki</a></li>
+                        <li><a  href="#" class="nav-link active" aria-current="page">Lifestyle</a></li>
+                        <li><a  href="#" class="nav-link active" aria-current="page">Odzież piłkarska</a></li>
+                        <li><a  href="#" class="nav-link active" aria-current="page"></a>Akcesoria</li>
+                    </ul>
+                </div>
                 <div class="products"></div>
             </div>
         </div>
