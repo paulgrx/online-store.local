deb57c419cc811ac9d5c513cfb85330cb2940be6
Paul <koncewiczpaul@gmail.com>
2024-12-09 22:44:55 +0100

    Css styles for mobile -v

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -5,9 +5,8 @@
         <meta name="viewport"
               content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
         <meta http-equiv="X-UA-Compatible" content="ie=edge">
-        <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/css/bootstrap.min.css">
+        <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet">
         <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.7.1/jquery.min.js"></script>
-        <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/js/bootstrap.min.js"></script>
 
         @vite(['resources/scss/app.scss'])
 
@@ -18,7 +17,7 @@
         <div class="content">
             <div class="header">
                 <div class="row">
-                    <div class="col-lg-6 col-md-6 col-sm-6">
+                    <div class="col-lg-6 col-md-6 col-sm-6 col-6">
                         <p><a href="/">Sklep</a></p>
                     </div>
                     <div class="col-lg-3 search-button">
@@ -31,31 +30,31 @@
                             </button>
                         </form>
                     </div>
-                    <div class="col-lg-3 col-md-6 col-sm-6 navi-buttons">
+                    <div class="col-lg-3 col-md-6 col-sm-6 col-6 navi-buttons">
                         <button class="log-in" onclick="location.href='{{route('form')}}'">
                             <svg xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" version="1.1" width="256" height="256" viewBox="0 0 256 256" xml:space="preserve" style="width: 20px; height: 20px;">
-                                <g style="stroke: none; stroke-width: 0; stroke-dasharray: none; stroke-linecap: butt; stroke-linejoin: miter; stroke-miterlimit: 10; fill: white; fill-rule: nonzero; opacity: 1;" transform="translate(1.4065934065934016 1.4065934065934016) scale(2.81 2.81)" >
-                                    <path d="M 85.347 90 c 0 -22.283 -18.064 -40.347 -40.347 -40.347 S 4.653 67.717 4.653 90 H 85.347 z" style="stroke: none; stroke-width: 1; stroke-dasharray: none; stroke-linecap: butt; stroke-linejoin: miter; stroke-miterlimit: 10; fill: rgb(0,0,0); fill-rule: nonzero; opacity: 1;" transform=" matrix(1 0 0 1 0 0) " stroke-linecap="round" />
-                                    <circle cx="45.003" cy="21.413" r="21.413" style="stroke: none; stroke-width: 1; stroke-dasharray: none; stroke-linecap: butt; stroke-linejoin: miter; stroke-miterlimit: 10; fill: rgb(0,0,0); fill-rule: nonzero; opacity: 1;" transform="  matrix(1 0 0 1 0 0) "/>
-                                </g>
-                            </svg>
+                            <g style="stroke: none; stroke-width: 0; stroke-dasharray: none; stroke-linecap: butt; stroke-linejoin: miter; stroke-miterlimit: 10; fill: white; fill-rule: nonzero; opacity: 1;" transform="translate(1.4065934065934016 1.4065934065934016) scale(2.81 2.81)" >
+                                <path d="M 85.347 90 c 0 -22.283 -18.064 -40.347 -40.347 -40.347 S 4.653 67.717 4.653 90 H 85.347 z" style="stroke: none; stroke-width: 1; stroke-dasharray: none; stroke-linecap: butt; stroke-linejoin: miter; stroke-miterlimit: 10; fill: rgb(0,0,0); fill-rule: nonzero; opacity: 1;" transform=" matrix(1 0 0 1 0 0) " stroke-linecap="round" />
+                                <circle cx="45.003" cy="21.413" r="21.413" style="stroke: none; stroke-width: 1; stroke-dasharray: none; stroke-linecap: butt; stroke-linejoin: miter; stroke-miterlimit: 10; fill: rgb(0,0,0); fill-rule: nonzero; opacity: 1;" transform="  matrix(1 0 0 1 0 0) "/>
+                            </g>
+                        </svg>
                         </button>
                         <button class="basket-logo" onclick="location.href='{{route('basket')}}'">
                             <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" fill="currentColor" class="bi bi-basket" viewBox="0 0 16 16" style="                 ">
                                 <path d="M5.757 1.071a.5.5 0 0 1 .172.686L3.383 6h9.234L10.07 1.757a.5.5 0 1 1 .858-.514L13.783 6H15a1 1 0 0 1 1 1v1a1 1 0 0 1-1 1v4.5a2.5 2.5 0 0 1-2.5 2.5h-9A2.5 2.5 0 0 1 1 13.5V9a1 1 0 0 1-1-1V7a1 1 0 0 1 1-1h1.217L5.07 1.243a.5.5 0 0 1 .686-.172zM2 9v4.5A1.5 1.5 0 0 0 3.5 15h9a1.5 1.5 0 0 0 1.5-1.5V9zM1 7v1h14V7zm3 3a.5.5 0 0 1 .5.5v3a.5.5 0 0 1-1 0v-3A.5.5 0 0 1 4 10m2 0a.5.5 0 0 1 .5.5v3a.5.5 0 0 1-1 0v-3A.5.5 0 0 1 6 10m2 0a.5.5 0 0 1 .5.5v3a.5.5 0 0 1-1 0v-3A.5.5 0 0 1 8 10m2 0a.5.5 0 0 1 .5.5v3a.5.5 0 0 1-1 0v-3a.5.5 0 0 1 .5-.5m2 0a.5.5 0 0 1 .5.5v3a.5.5 0 0 1-1 0v-3a.5.5 0 0 1 .5-.5"/>
                             </svg>
                         </button>
-                        <button class="mobile-list">
+                        <button class="mobile-list d-lg-none" data-bs-toggle="offcanvas" data-bs-target="#categoriesMenu" aria-controls="categoriesMenu">
                             <svg xmlns="http://www.w3.org/2000/svg" width="25" height="25" fill="currentColor" class="bi bi-list" viewBox="0 0 16 16">
                                 <path fill-rule="evenodd" d="M2.5 12a.5.5 0 0 1 .5-.5h10a.5.5 0 0 1 0 1H3a.5.5 0 0 1-.5-.5m0-4a.5.5 0 0 1 .5-.5h10a.5.5 0 0 1 0 1H3a.5.5 0 0 1-.5-.5m0-4a.5.5 0 0 1 .5-.5h10a.5.5 0 0 1 0 1H3a.5.5 0 0 1-.5-.5"/>
                             </svg>
                         </button>
                     </div>
                     <form action="{{ route('search') }}" method="get" role="search">
-                        <div class="col-sm-12 mobile-search-button">
+                        <div class="mobile-search-button">
                             <div class="input-group">
-                                <input type="search" name="search" class="product-search col-md-11" placeholder="Szukaj" value="{{ isset($searchQuery) ? $searchQuery : '' }}" aria-label="Szukaj">
-                                <button type="submit" class="search-button-btn col-md-1">
+                                <input type="search" name="search" class="product-search" placeholder="Szukaj" value="{{ isset($searchQuery) ? $searchQuery : '' }}" aria-label="Szukaj">
+                                <button type="submit" class="search-button-btn">
                                     <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" fill="currentColor" class="bi bi-search" viewBox="0 0 16 16">
                                         <path d="M11.742 10.344a6.5 6.5 0 1 0-1.397 1.398h-.001q.044.06.098.115l3.85 3.85a1 1 0 0 0 1.415-1.414l-3.85-3.85a1 1 0 0 0-.115-.1zM12 6.5a5.5 5.5 0 1 1-11 0 5.5 5.5 0 0 1 11 0"/>
                                     </svg>
@@ -65,7 +64,6 @@
                     </form>
                 </div>
             </div>
-
 
             <div class="main-content">
                 @include('sidebar', ['categoriesData' => $categoriesData])
@@ -81,7 +79,6 @@
                         </div>
                     </div>
                     <div class="row">
-
                         @if($data->isEmpty())
                             @if($isSearch)
                                 <p class="search-alert">Nic nie znaleziono po twoim wyszukiwaniu: <strong>{{$searchQuery}}</strong></p>
@@ -91,12 +88,14 @@
                         @else
                             @foreach($data as $item)
                                 <a href="{{route('product', ['id' => $item->id])}}" class="col-lg-12 outcome">
-                                    <div class="col-sm-2">
-                                        <img src="images/{{$item->image}}" alt="Photo" class="product-photo">
-                                    </div>
-                                    <div class="col-sm-10">
-                                        <h2>{{$item->name}}</h2>
-                                        <p>{{$item->text}}</p>
+                                    <div class="product-info">
+                                        <div class="col-sm-2">
+                                            <img src="images/{{$item->image}}" alt="Photo" class="product-photo">
+                                        </div>
+                                        <div class="col-sm-10">
+                                            <h2>{{$item->name}}</h2>
+                                            <p>{{$item->text}}</p>
+                                        </div>
                                     </div>
                                     <div class="purchase">
                                         <h3>{{ number_format($item->price, 2, ',', ' ') }} zł</h3>
@@ -114,6 +113,9 @@
                 </div>
             </div>
         </div>
+
+        <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.bundle.min.js"></script>
+
     </body>
 </html>
 
--- resources/views/sidebar.blade.php
+++ resources/views/sidebar.blade.php
@@ -12,3 +12,24 @@
         </ul>
     </div>
 </div>
+
+
+<div class="col-2 mobile-aside offcanvas offcanvas-start" tabindex="-1" id="categoriesMenu" aria-labelledby="categoriesMenuLabel">
+    <div class="aside-in offcanvas-header">
+        <div class="col-2"></div>
+        <h2 class="offcanvas-title col-8" id="categoriesMenuLabel">Kategorie</h2>
+        <button type="button" class="btn-close col-2" data-bs-dismiss="offcanvas" aria-label="Close"></button>
+    </div>
+    <div class="offcanvas-body">
+        <ul class="list-group">
+            @foreach($categoriesData as $category)
+                <li>
+                    <a href="{{ url('/search?' . http_build_query(['category' => $category->id, 'sort_by' => $currentSortBy])) }}"
+                       class="nav-link {{ $currentCategoryId == $category->id ? 'active' : '' }}">{{ $category->categories}}</a>
+                </li>
+            @endforeach
+        </ul>
+    </div>
+</div>
+
+
